aa1d235f9921988b21b5086d42266c15c559b822
Casey Scarborough <caseyscarborough@gmail.com>
2013-09-12 19:17:08 -0400

    Refactor to make better sense logically.

--- src/puzzle.java
+++ src/puzzle.java
@@ -8,19 +8,15 @@ public class puzzle {
   int[] initialState = new int[9];
   int[] state = new int[9];
 
-  public puzzle(String t, String m, String b) {
-    this.initialState = convertRowsToArray(t, m, b);
+  public puzzle(int[] puzzleInput) {
+    this.initialState = puzzleInput;
     this.state = this.initialState;
   }
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    int[] puzzleInput = getConsoleInput();
 
-    String topRow = scanner.nextLine();
-    String middleRow = scanner.nextLine();
-    String bottomRow = scanner.nextLine();
-
-    puzzle puzzle = new puzzle(topRow, middleRow, bottomRow);
+    puzzle puzzle = new puzzle(puzzleInput);
 
     if (isSolvable(puzzle.initialState)) {
       System.out.println("Puzzle is solvable.");
@@ -43,30 +39,32 @@ public class puzzle {
     return (inversions % 2 == 0);
   }
 
-  public static int[] convertRowsToArray(String t, String m, String b) {
-    // Convert String into arrays
-    String[] topRow    = t.split(" ");
-    String[] middleRow = m.split(" ");
-    String[] bottomRow = b.split(" ");
-    String[] s = new String[9];
+  public static boolean isValid(String puzzleInput) {
+    return (puzzleInput.length() == 17 && !puzzleInput.contains("9"));
+  }
 
-    try {
-      System.arraycopy(topRow,    0, s, 0, 3);
-      System.arraycopy(middleRow, 0, s, 3, 3);
-      System.arraycopy(bottomRow, 0, s, 6, 3);
-    } catch (ArrayIndexOutOfBoundsException e) {
+  public static int[] getConsoleInput() {
+    Scanner scanner = new Scanner(System.in);
+
+    String t = scanner.nextLine().trim();
+    String m = scanner.nextLine().trim();
+    String b = scanner.nextLine().trim();
+
+    String puzzle = String.format("%s %s %s", t, m, b);
+    return convertToArray(puzzle);
+  }
+
+  public static int[] convertToArray(String s) {
+    if (!isValid(s)) {
       System.out.println("Invalid 8-puzzle entered!");
       System.exit(0);
     }
 
     int[] p = new int[9];
-    for(int i = 0; i < s.length; i++) {
-      p[i] = Integer.parseInt(s[i]);
-      if (p[i] > 8) {
-        System.out.println("Invalid 8-puzzle entered!");
-        System.exit(0);
-      }
-    }
+    // Remove spaces from string.
+    s = s.replace(" ", "");
+    for(int i = 0; i < s.length(); i++)
+      p[i] = Integer.parseInt(Character.toString(s.charAt(i)));
     return p;
   }
 

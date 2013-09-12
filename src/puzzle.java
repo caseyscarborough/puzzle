21bd3f5c683f1890de505ab1fc6be3364ed19b07
Casey Scarborough <caseyscarborough@gmail.com>
2013-09-12 18:59:25 -0400

    Add basic input, output, and checking solvability.

--- src/puzzle.java
+++ src/puzzle.java
@@ -1,28 +1,82 @@
+import com.sun.deploy.util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class puzzle {
 
-  public static void main(String[] args) {
-    int[] p = { 1, 4, 5, 7, 6, 2, 3, 8 };
-    if(isSolvable(p)) {
-      System.out.println("Yes");
-    } else {
-      System.out.println("No");
-    }
+  int[] initialState = new int[9];
+  int[] state = new int[9];
+
+  public puzzle(String t, String m, String b) {
+    this.initialState = convertRowsToArray(t, m, b);
+    this.state = this.initialState;
   }
 
-  public static boolean isSolvable(int [] p) {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String topRow = scanner.nextLine();
+    String middleRow = scanner.nextLine();
+    String bottomRow = scanner.nextLine();
+
+    puzzle puzzle = new puzzle(topRow, middleRow, bottomRow);
+
+    if (isSolvable(puzzle.initialState)) {
+      System.out.println("Puzzle is solvable.");
+    } else {
+      System.out.println("Puzzle is NOT solvable!");
+    }
+
+    System.out.println(puzzle.toString());
+  }
+
+  public static boolean isSolvable(int[] p) {
     int inversions = 0;
 
     for(int i = 0; i < p.length - 1; i++) {
-      for(int j = i + 1; j < p.length; j++) {
-        if(p[i] > p[j]) {
-          System.out.printf("(%d, %d)\n", p[i], p[j]);
-          inversions++;
-        }
-      }
-    }
+      for(int j = i + 1; j < p.length; j++)
+        if(p[i] > p[j]) inversions++;
 
-    System.out.println("Number of inversions are: " + inversions);
-    return !((inversions > 0) && (inversions % 2 == 0));
+      if(p[i] == 0 && i % 2 == 1) inversions++;
+    }
+    return (inversions % 2 == 0);
   }
 
+  public static int[] convertRowsToArray(String t, String m, String b) {
+    // Convert String into arrays
+    String[] topRow    = t.split(" ");
+    String[] middleRow = m.split(" ");
+    String[] bottomRow = b.split(" ");
+    String[] s = new String[9];
+
+    try {
+      System.arraycopy(topRow,    0, s, 0, 3);
+      System.arraycopy(middleRow, 0, s, 3, 3);
+      System.arraycopy(bottomRow, 0, s, 6, 3);
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Invalid 8-puzzle entered!");
+      System.exit(0);
+    }
+
+    int[] p = new int[9];
+    for(int i = 0; i < s.length; i++) {
+      p[i] = Integer.parseInt(s[i]);
+      if (p[i] > 8) {
+        System.out.println("Invalid 8-puzzle entered!");
+        System.exit(0);
+      }
+    }
+    return p;
+  }
+
+  public String toString() {
+    int[] state = this.state;
+    String s = "";
+    for(int i = 0; i < state.length; i++) {
+      if(i % 3 == 0) s += "\n";
+      s += String.format("%d ", state[i]);
+    }
+    return s;
+  }
 }

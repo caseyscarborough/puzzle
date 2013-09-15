a2d6194e6caf1108d58ef14272fabdf3f1743e2c
Casey Scarborough <caseyscarborough@gmail.com>
2013-09-15 01:48:29 -0400

    Add puzzle.sh for easier execution.

---
+++ puzzle.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+JAVA=$(which java)
+if [ $? -eq 1 ]; then
+  echo "Cannot find the java command in your PATH."
+  exit
+fi
+
+JAR="target/Puzzle.jar"
+if [ ! -f ${JAR} ]; then
+  echo "Cannot find ${JAR}."
+
+  MVN=$(which mvn)
+  if [ $? -eq 1 ]; then
+    echo "Cannot find Maven on your system."
+    exit
+  fi
+  echo "Packaging ${JAR}..."
+  exec mvn package -P cli-dist
+fi
+
+exec ${JAVA} -cp ${JAR} com.caseyscarborough.puzzle.Puzzle
